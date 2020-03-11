5e4a02fdb38f01fad0307a9a50c359292d6c2a9c
Haggreed <hugo.duvegheant@epita.fr>
2020-03-11 16:15:35 +0000

    part 1 exercises

---
+++ app.js
@@ -0,0 +1,24 @@
+
+var firstName = "Hugo";
+
+var favoriteColor = "#6a0dad";
+
+var favoriteFood = "Pizza";
+var city = "Dublin";
+
+console.log(firstName);
+console.log(favoriteColor);
+console.log(favoriteFood);
+
+console.log(firstName+"\n"+favoriteColor+"\n"+favoriteFood);
+var phrase = "A rainbow is formed by the sunglight. Shining thorugh rain drops so small. The rain bends the light form the sunshine, and colors the sky for us all.";
+var endingPhrase = "is what we see. Rainbow colours, are made with light bending you see";
+var one = "red";
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six = "indigo";
+var seven = "violet"; 
+
+console.log(phrase +" "+one+" "+two+" "+three+" "+four+" "+five+" "+six+" "+seven+" "+endingPhrase);
